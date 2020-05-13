c6e1ccd3d1259fd963920d22e2eb6c89e28dac06
Brennan Kolotinsky <BrennanKolotinsky@gmail.com>
2020-05-13 09:28:36 -0400

    using CIDR now with bit masking to calc IP ranges

--- createIPSQL.php
+++ createIPSQL.php
@@ -108,12 +108,10 @@ class createSql {
 				if ($test && $row > 100)
 					break;
 
-				$longIp = ip2long(str_replace("/","",$data[0]));
+				$longIpRange = $this->cidrToRange($data[0]);
+				$minRange = $longIpRange[0];
 
-				//echo $longIp;
-				//echo "\n";
-				$correspondingLocationId = 0;
-				$correspondingLocationId = $this->binarySearch($data[0]);
+				$correspondingLocationId = $this->binarySearch($minRange, $row);
 
 				$sql = "INSERT INTO IPAddress (Id, Network, GeonameId, ContinentCode, ContinentName, CountryISOCode, CountryName, isAnonymousProxy, isSatelliteProvider, LocationId) \nVALUES ({$row}, '{$data[0]}', {$data[1]}, '{$data[2]}', '{$data[3]}', '{$data[4]}', '{$data[5]}', {$data[6]}, {$data[7]}, {$correspondingLocationId});\n\n";
 
@@ -128,14 +126,30 @@ class createSql {
 	}
 
 	/*
+	This function takes in a IPv4 String value and returns a long range using bit masking
+	The code was taken from https://stackoverflow.com/questions/4931721/getting-list-ips-from-cidr-notation-in-php
+	Input:
+		String ip address (convert from String first)
+	Output:
+		Returns the ipRange
+	*/
+	function cidrToRange($cidr) {
+	  	$range = array();
+		$cidr = explode('/', $cidr);
+		$range[0] = long2ip((ip2long($cidr[0])) & ((-1 << (32 - (int)$cidr[1]))));
+		$range[1] = long2ip((ip2long($range[0])) + pow(2, (32 - (int)$cidr[1])) - 1);
+		return $range;
+	}
+
+	/*
 	This function searches for the appropriate location that associates with this addresses
 	We use binary search to reduce runtime from O(N) to O(logN) -- a significant improvement in large datasets
 	Input:
-		Long ip address (convert from String first)
+		Long ip address low range
 	Output:
 		Int location id
 	*/
-	function binarySearch($ip4) {
+	function binarySearch($ip4LowRange, $row) {
 
 		$lo = 0;
 		$hi = sizeof($this->minIp);
@@ -144,15 +158,13 @@ class createSql {
 
 			$mid = (int) ($lo + ($hi - $lo) / 2); // prevents overflow! (rather than (lo + hi) / 2)
 
-			if ($this->minIp[$mid] <= $ip4 && $this->maxIp[$mid] >= $ip4)
+			if ($this->minIp[$mid] === $ip4LowRange)
 				return $mid + 1;
 			# we are less than the current point
-			else if ($this->minIp[$mid] < $ip4)
+			else if ($this->minIp[$mid] < $ip4LowRange)
 				$lo = $mid + 1;
-			else if ($this->maxIp[$mid] > $ip4)
-				$hi = $mid - 1;
 			else
-				echo "here";
+				$hi = $mid - 1;
 		}
 
 		return $lo + 1; // +1 because the ID starts at 1 (not zero)
@@ -173,5 +185,5 @@ class createSql {
 }
 
 $obj = new createSql();
-$obj->test();
-#$obj->buildSQL();
+// $obj->test();
+$obj->buildSQL();
