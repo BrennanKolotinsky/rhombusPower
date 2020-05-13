1f1cc7efb8db67b778ead00da8e1237e85c0af51
Brennan Kolotinsky <BrennanKolotinsky@gmail.com>
2020-05-13 06:41:12 -0400

    schema for the database tables

---
+++ schemas.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IPAddress (
+    Id int AUTO_INCREMENT,
+    Network varchar(255) NOT NULL,
+    GeonameId int,
+    ContinentCode varchar(2),
+    ContinentName varchar(128),
+    CountryISOCode varchar(2),
+    CountryName varchar(128),
+    isAnonymousProxy boolean DEFAULT 0,
+    isSatelliteProvider boolean DEFAULT 0,
+    LocationId int, # each IP belongs to a location many ips to one location!
+
+    FOREIGN KEY (LocationId) REFERENCES Location(Id), # allows for joins, showing information about the location
+
+    PRIMARY KEY (Id)
+);
+
+CREATE TABLE Location (
+    Id int AUTO_INCREMENT,
+    IPFrom int,
+    IPTo int,
+    CountryISOCode varchar(2),
+    Country varchar(2),
+    State varchar(128),
+    City varchar(2),
+    Latitude decimal(18, 6), # max 6 ints to the right of the decimal!
+    Longitude decimal(18, 6),
+
+    PRIMARY KEY (Id)
+);
